75d3a88cb6067fcfa862cd995f25f15d0be5c384
logicerpsolution <noreply>
2016-02-01 06:34:27 +0000

    Add component sigunup

---
+++ ui/sigunup.reel/sigunup.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/sigunup.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Sigunup
+ * @extends Component
+ */
+exports.Sigunup = Component.specialize(/** @lends Sigunup# */ {
+    constructor: {
+        value: function Sigunup() {
+            this.super();
+        }
+    }
+});
